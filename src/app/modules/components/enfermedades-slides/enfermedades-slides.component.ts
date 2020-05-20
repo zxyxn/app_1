a3aaf124c04f54a68c436e133a4452ea07bab229
Juan Solorio <juan.solorio.rayo@gmail.com>
2020-05-20 15:30:17 -0600

    Se creo la funcion para obtener las enfermedades en el home page

--- src/app/modules/components/enfermedades-slides/enfermedades-slides.component.ts
+++ src/app/modules/components/enfermedades-slides/enfermedades-slides.component.ts
@@ -9,17 +9,21 @@ import { Enfermedades } from 'src/app/interfaces/interfaces';
   styleUrls: ['./enfermedades-slides.component.scss'],
 })
 export class EnfermedadesSlidesComponent implements OnInit {
+  encabezado: any;
   enfermedades: any;
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    
+    // Devuelve -> encabezado: arrreglo & enfermedades: arreglo de objetos
+    // Puede ser utilizado en el HTML
     this.dataService.getInformacionEnfermedades().subscribe((value) => {
-      this.enfermedades = Object.keys(value)
+      this.encabezado = Object.values(value)[0];
+      this.encabezado = Object.values(this.encabezado);
+      console.log(this.encabezado);
+      let result = Object.values(value)[1];
+      this.enfermedades = Object.values(result);
+      this.enfermedades = Object.values(this.enfermedades);
       console.log(this.enfermedades);
-      console.log();
-
     })
   }
-  
 }
